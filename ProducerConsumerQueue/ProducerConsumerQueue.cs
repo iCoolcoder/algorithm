f488bbc8755f9bbd56cb6b0101ae601cade2bc9a
iCoolcoder <canzhanghys@gmail.com>
2013-12-09 19:03:05 +0800

    Add ProducerConsumerQueue project
    In this project, it has a good example of producer and consumer class.

---
+++ ProducerConsumerQueue/ProducerConsumerQueue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ProducerConsumerQueue
+{
+    class ProducerConsumerQueue : IDisposable
+    {
+        private EventWaitHandle _wh = new AutoResetEvent(false);
+        private Thread _worker;
+        private readonly object _locker = new object();
+        private Queue<string> _tasks = new Queue<string>();
+
+        public ProducerConsumerQueue()
+        {
+            _worker = new Thread(Work);
+            _worker.Start();
+        }
+
+        public void EnqueueTask(string task)
+        {
+            lock (_locker)
+            {
+                _tasks.Enqueue(task);
+                _wh.Set();
+            }
+        }
+
+        public void Dispose()
+        {
+            EnqueueTask(null);
+            _worker.Join();
+            _wh.Close();
+        }
+
+        void Work()
+        {
+            while (true)
+            {
+                string task = null;
+                lock (_locker)
+                {
+                    if (_tasks.Count > 0)
+                    {
+                        task = _tasks.Dequeue();
+                        if(task == null) return;
+                    }
+                }
+                if (task != null)
+                {
+                    Console.WriteLine("Performing task: " + task);
+                    Thread.Sleep(1000); // simulate work ...
+                }
+                else
+                {
+                    _wh.WaitOne();
+                }
+            }
+        }
+    }
+}
---
+++ ProducerConsumerQueue/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ProducerConsumerQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ProducerConsumerQueue q = new ProducerConsumerQueue())
+            {
+                q.EnqueueTask("Hello");
+                for(int i = 0; i < 10; i++) q.EnqueueTask("Say " + i);
+                q.EnqueueTask("GoodBye!");
+            }
+        }
+    }
+}
