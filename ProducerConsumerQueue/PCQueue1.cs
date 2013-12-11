652cfa9cec1c863dd4bc2057cc48479117ebd321
iCoolcoder <canzhanghys@gmail.com>
2013-12-11 18:10:48 +0800

    Add PCQueue1.cs
    this class implement producer and consumer using task class of c#.

---
+++ ProducerConsumerQueue/PCQueue1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducerConsumerQueue
+{
+    class PCQueue1 : IDisposable
+    {
+        class WorkItem
+        {
+            public readonly TaskCompletionSource<object> TaskSource;
+            public readonly Action Action;
+            public readonly CancellationToken? CancelToken;
+
+            public WorkItem(TaskCompletionSource<object> taskSource, Action action, CancellationToken? cancelToken)
+            {
+                TaskSource = taskSource;
+                Action = action;
+                CancelToken = cancelToken;
+            }
+        }
+
+        BlockingCollection<WorkItem> _taskQ = new BlockingCollection<WorkItem>();
+
+        public PCQueue1(int workerCount)
+        {
+            // create and start a separate Task for each consumer:
+            for (int i = 0; i < workerCount; i++)
+            {
+                Task.Factory.StartNew(Consume);
+            }
+        }
+
+        public void Dispose() { _taskQ.CompleteAdding(); }
+
+        public Task EnqueueTask(Action action)
+        {
+            return EnqueueTask(action, null);
+        }
+
+        public Task EnqueueTask(Action action, CancellationToken? cancelToken)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            _taskQ.Add(new WorkItem(tcs, action, cancelToken));
+            return tcs.Task;
+        }
+
+        private void Consume()
+        {
+            foreach (var workItem in _taskQ.GetConsumingEnumerable())
+            {
+                if (workItem.CancelToken.HasValue && workItem.CancelToken.Value.IsCancellationRequested)
+                {
+                    workItem.TaskSource.SetCanceled();
+                }
+                else
+                {
+                    try
+                    {
+                        workItem.Action();
+                        workItem.TaskSource.SetResult(null); //Indicate completion
+                    }
+                    catch (Exception ex)
+                    {
+                        workItem.TaskSource.SetException(ex);
+                    }
+                }
+            }
+        }
+    }
+}
