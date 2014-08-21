f1aaef898cff48465b0b3bf3afacb170fdff203c
iCoolcoder <canzhanghys@gmail.com>
2014-08-21 17:00:22 +0800

    Image Comparison based on pHash
    pHash get all the eigenvalue and produce hash value for each picture.
    Then compare the threshold value with the D-value.

---
+++ Sample/Comparison/Comparison/program.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "pHash.h"
+#define _PHASH_H 1
+#define HAVE_IMAGE_HASH 1
+
+using namespace std;
+
+int main()
+{
+	/*ulong64 a;
+	ulong64 b;
+	
+	ph_dct_imagehash("", a);
+	ph_dct_imagehash("", b);
+	
+	if(ph_hamming_distance(a, b) > 0.5)
+	{
+		cout<<"success"<<endl;
+	}*/
+	return 0;
+}
