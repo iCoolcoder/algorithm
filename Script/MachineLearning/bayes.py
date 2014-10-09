ebcbdee3f09401d0c4b5bcb7dfb7830c104e91a6
zhangcan <zhangcan@youku.com>
2014-10-09 15:14:22 +0800

    Optimize bayes training algorithm

--- Script/MachineLearning/bayes.py
+++ Script/MachineLearning/bayes.py
@@ -1,6 +1,11 @@
+from numpy import *
 def loadDataSet():
 	postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
-	['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid']]
+	['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+	['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+	['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+	['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+	['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
 	classVec = [0, 1, 0, 1, 0, 1]
 	return postingList, classVec
 	
@@ -23,9 +28,9 @@ def trainNB0(trainMatrix, trainCategory):
 	numTrainDocs = len(trainMatrix)
 	numWords = len(trainMatrix[0])
 	pAbusive = sum(trainCategory) / float(numTrainDocs)
-	p0Num = zeros(numWords)
-	p1Num = zeros(numWords)
-	p0Denom = 0.0; p1Denom = 0.0
+	p0Num = ones(numWords)
+	p1Num = ones(numWords)
+	p0Denom = 2.0; p1Denom = 2.0
 	for i in range(numTrainDocs):
 		if trainCategory[i] == 1:
 			p1Num += trainMatrix[i]
@@ -33,8 +38,8 @@ def trainNB0(trainMatrix, trainCategory):
 		else:
 			p0Num += trainMatrix[i]
 			p0Denom += sum(trainMatrix[i])
-	p1Vect = p1Num / p1Denom
-	p0Vect = p0Num / p0Denom
+	p1Vect = log(p1Num / p1Denom)
+	p0Vect = log(p0Num / p0Denom)
 	return p0Vect, p1Vect, pAbusive
 	
 def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
@@ -45,8 +50,22 @@ def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
 	else:
 		return 0
 		
-		
+def testingNB():
+	listOPosts, listClasses = loadDataSet()
+	myVocabList = createVocabList(listOPosts)
+	trainMat  = []
+	for postinDoc in listOPosts:
+		trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+	p0V, p1V, pAb = trainNB0(array(trainMat), array(listClasses))
+	testEntry = ['love', 'my', 'dalmation']
+	thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+	print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+	testEntry = ['stupid', 'garbage']
+	thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+	print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+
+
 listOPosts, listClasses = loadDataSet()
 myVocabList = createVocabList(listOPosts)
-print myVocabList
-	
+print myVocabList	
+testingNB()
